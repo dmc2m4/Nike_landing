3f054e5c82e059d2b4d51035a473e593671bd5ce
dmc2m4 <danielmartinezcabrera@hotmail.com>
2024-04-03 14:22:49 -0500

    revert test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-3xl underline">Hello World 123</h1>
+    <h1 className="text-bold text-3xl underline">Hello World</h1>
   );
 }
