d4391a944f23ef6a23ddf458e7c6e7adad83b95f
dmc2m4 <danielmartinezcabrera@hotmail.com>
2024-04-15 10:33:56 -0500

    start Hero component

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 border-red-500 p-2"
+    ></section>
+  );
 };
 
 export default Hero;
